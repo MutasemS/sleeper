2346b3c7a4ba708f1c43dd45d97e854fde2e771e
RyanDaniels2025 <ryan.daniels1019@gmail.com>
2024-12-07 12:36:01 -0500

    Correct link.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Landing() {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="/categories"
+              href="/edit-categories"
             >
               <h3 className="text-2xl font-bold">Edit Categories →</h3>
               <div className="text-lg">Click here to edit or update categories</div>
