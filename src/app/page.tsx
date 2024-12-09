12701f27f5620e9d1454a2665b310315d987858b
Mutasem Salem <ms5268@drexel.edu>
2024-12-09 15:58:21 -0500

    name change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Landing() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            <span className="text-[hsl(280,100%,70%)]">Budget Tracker</span>
+            <span className="text-[hsl(280,100%,70%)]">Sleeper</span>
           </h1>
 
           <div className="mb-8 w-full max-w-4xl">
